badd127194ba40910d2483f20c994cb938bad03d
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-17 21:29:59 +0530

    new browser added

---
+++ src/test/java/apnacollage/Chrome.java
@@ -0,0 +1,25 @@
+package apnacollage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Chrome {
+
+    WebDriver driver;
+
+    @Test
+    void chromesetup() {
+
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+
+        driver.get("https://fb.com");
+
+        driver.quit();
+    }
+
+}
